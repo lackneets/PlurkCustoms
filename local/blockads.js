c6a939d366cb00cb0f32fed3569630480c71d887
Lackneets <lackneets@gmail.com>
2015-04-12 21:53:21 +0800

    1.7.8.2 hide cbox ads

--- local/blockads.js
+++ local/blockads.js
@@ -18,6 +18,34 @@ BlockAdsPlugin.prototype.stop = function(){
 }
 BlockAdsPlugin.prototype.hideAd = function(thisArg){
   return function(){
-    thisArg.enabled && $(this).hide();
+    if(thisArg.enabled){
+      $(this).hide();
+      fixCboxEmpty(this);
+    }
   }
+}
+
+function fixCboxEmpty(element){
+  if($(element).is('.cbox_ads')){
+    setTimeout(function(){fixBlockHeight($(element).siblings('.cbox_response'))}, 200);
+    setTimeout(function(){fixBlockHeight($(element).siblings('.cbox_response'))}, 300);
+    setTimeout(function(){fixBlockHeight($(element).siblings('.cbox_response'))}, 500);
+  }
+}
+
+function fixBlockHeight(element){
+  var siblingsHeight = 0;
+
+  if($(element).length == 0){
+    return false;
+  }
+
+  $(element).siblings().each(function(){
+    if(window.getComputedStyle(this).display == 'block' && window.getComputedStyle(this).position != 'absolute'){
+      siblingsHeight += $(this).outerHeight(true);
+    }
+  });
+
+  var paddings = parseFloat($(element).css('padding-top')) + parseFloat($(element).css('padding-bottom'));
+  $(element).height($(element).parent().height() - siblingsHeight - paddings);
 }
